137707dc5ea10b8911b8715a98c99cb454dcccc5
Seb <sprogers88@gmail.com>
2017-05-08 22:59:40 +0100

    Added Tests around Api Group Service

--- spreaders.lib.tests/ApiGroupServiceTests.cs
+++ spreaders.lib.tests/ApiGroupServiceTests.cs
@@ -2,6 +2,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using spreaders.lib.Services;
 using spreaders.lib.Models.Dtos.Json;
+using spreaders.lib.models.Entities;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace spreaders.lib.tests
 {
@@ -52,5 +55,116 @@ namespace spreaders.lib.tests
 
       Assert.IsNotNull(model.Transactions);
     }
+
+    [TestMethod]
+    public void ApiGroupService_GetReturnModel_GroupNameSet_ReturnedModelContainsGroupName()
+    {
+      ApiGroupService service = Setup();
+      Group group = new Group { Name = "Test1" };
+
+      ApiGetGroupReturnModel model = service.GetReturnModel(group);
+
+      Assert.AreEqual("Test1", model.Group.Name);
+    }
+
+    [TestMethod]
+    public void ApiGroupService_GetReturnModel_GroupIdSet_ReturnedModelContainsGroupId()
+    {
+      ApiGroupService service = Setup();
+      Guid expectedId = new Guid("88888888-4444-4444-4444-222222222222");
+      Group group = new Group { Id = expectedId };
+
+      ApiGetGroupReturnModel model = service.GetReturnModel(group);
+
+      Assert.AreEqual(expectedId, model.Group.Id);
+    }
+
+    [TestMethod]
+    public void ApiGroupService_GetReturnModel_GroupHas1Transaction_ReturnedModelContains1Transaction()
+    {
+      ApiGroupService service = Setup();
+      List<Transaction> transactions = new List<Transaction> { new Transaction() };
+      Group group = new Group { Transactions = transactions };
+
+      ApiGetGroupReturnModel model = service.GetReturnModel(group);
+
+      Assert.AreEqual(1, model.Transactions.Count);
+    }
+
+    [TestMethod]
+    public void ApiGroupService_GetReturnModel_GroupHas2Transactions_ReturnedModelContains2Transactions()
+    {
+      ApiGroupService service = Setup();
+      List<Transaction> transactions = new List<Transaction> { new Transaction(), new Transaction() };
+      Group group = new Group { Transactions = transactions };
+
+      ApiGetGroupReturnModel model = service.GetReturnModel(group);
+
+      Assert.AreEqual(2, model.Transactions.Count);
+    }
+
+    [TestMethod]
+    public void ApiGroupService_GetReturnModel_TransactionWithAmount10_ReturnedModelContainsTransactionWithAmount10()
+    {
+      ApiGroupService service = Setup();
+      List<Transaction> transactions = new List<Transaction> { new Transaction { Amount = 10 }};
+      Group group = new Group { Transactions = transactions };
+
+      ApiGetGroupReturnModel model = service.GetReturnModel(group);
+
+      Assert.AreEqual(10, model.Transactions.FirstOrDefault().Amount);
+    }
+
+    [TestMethod]
+    public void ApiGroupService_GetReturnModel_TransactionWithAmount12_ReturnedModelContainsTransactionWithAmount12()
+    {
+      ApiGroupService service = Setup();
+      List<Transaction> transactions = new List<Transaction> { new Transaction { Amount = 12 } };
+      Group group = new Group { Transactions = transactions };
+
+      ApiGetGroupReturnModel model = service.GetReturnModel(group);
+
+      Assert.AreEqual(12, model.Transactions.FirstOrDefault().Amount);
+    }
+
+    [TestMethod]
+    public void ApiGroupService_GetReturnModel_TransactionWith1Payee_ReturnedModelContainsTransactionWith1Payee()
+    {
+      ApiGroupService service = Setup();
+      List<Transaction> transactions = new List<Transaction> { new Transaction { Payees = new List<Person> { new Person() } } };
+      Group group = new Group { Transactions = transactions };
+
+      ApiGetGroupReturnModel model = service.GetReturnModel(group);
+
+      Assert.AreEqual(1, model.Transactions.FirstOrDefault().Payees.Count);
+    }
+
+    [TestMethod]
+    public void ApiGroupService_GetReturnModel_TransactionWithPayee_ReturnedModelContainsTransactionWithCorrectPayeeGuid()
+    {
+      ApiGroupService service = Setup();
+      Guid expectedId = new Guid("88888888-4444-4444-4444-222222222222");
+      List<Transaction> transactions = new List<Transaction> { new Transaction { Payees = new List<Person> { new Person { Id = expectedId } } } };
+      Group group = new Group { Transactions = transactions };
+
+      ApiGetGroupReturnModel model = service.GetReturnModel(group);
+
+      Assert.AreEqual(expectedId, model.Transactions.First().Payees.First());
+    }
+
+    [TestMethod]
+    public void ApiGroupService_GetReturnModel_TransactionWithPayer_ReturnedModelContainsTransactionWithCorrectPayerGuid()
+    {
+      ApiGroupService service = Setup();
+      Guid expectedId = new Guid("88888888-4444-4444-4444-222222222222");
+      List<Transaction> transactions = new List<Transaction> { new Transaction { Payer = new Person { Id = expectedId } } };
+      Group group = new Group { Transactions = transactions };
+
+      ApiGetGroupReturnModel model = service.GetReturnModel(group);
+
+      Assert.AreEqual(expectedId, model.Transactions.First().PayerId);
+    }
+
+
   }
 }
--- spreaders.lib/Services/ApiGroupService.cs
+++ spreaders.lib/Services/ApiGroupService.cs
@@ -20,7 +20,43 @@ namespace spreaders.lib.Services
     public ApiGetGroupReturnModel GetReturnModel(Group group)
     {
       ApiGetGroupReturnModel model = new ApiGetGroupReturnModel();
+      if(group != null)
+      {
+        model.Group = MapToJsonGroup(group);
+        model.Transactions = MapToJsonTransactions(group.Transactions);
+      }
       return model;
+    }
+
+    private JsonGroup MapToJsonGroup(Group group)
+    {
+      return new JsonGroup
+      {
+        Id = group.Id,
+        Name = group.Name
+      };
+    }
+
+    private List<JsonTransaction> MapToJsonTransactions(ICollection<Transaction> transactions)
+    {
+      List<JsonTransaction> jsonTransactions = new List<JsonTransaction>();
+      foreach(Transaction transaction in transactions)
+        jsonTransactions.Add(MapToJsonTransaction(transaction));
+      return jsonTransactions;
+    }
+
+    private JsonTransaction MapToJsonTransaction(Transaction transaction)
+    {
+      return new JsonTransaction
+      {
+        Id = transaction.Id,
+        Amount = transaction.Amount,
+        Deleted = transaction.Deleted,
+        Description = transaction.Description,
+        GroupId = transaction.GroupId,
+        Payees = transaction.Payees.Select(x => x.Id).ToList()
+        
+      };
     }
   }
 }
